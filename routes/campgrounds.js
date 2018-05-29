9a3ee085a6a7de6ea125fcf99dcce8055041dc7e
Pranay Maharana <pranay@Pranays-MBP.tampabay.rr.com>
2018-05-28 22:37:02 -0400

    passport authorization added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -46,11 +46,56 @@ router.post('/', isLoggedIn, function(req, res) {
   });
 })
 
+router.get('/:id/edit', checkCampgroundOwnership, function(req, res) {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    res.render('campgrounds/edit', {campground: foundCampground});
+  });
+})
+
+router.put('/:id', function(req, res) {
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updated) {
+    if (err) {
+      res.redirect('/campgrounds');
+    } else {
+      res.redirect(`/campgrounds/${updated._id}`);
+    }
+  });
+})
+
+router.delete('/:id', checkCampgroundOwnership, function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect('/campgrounds');
+    } else {
+      res.redirect('/campgrounds');
+    }
+  });
+})
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/login');
+}
+
+function checkCampgroundOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+      if (err) {
+        res.redirect('back');
+      }
+      else {
+        if (foundCampground.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect('back');
+        }
+      }
+    });  
+  } else {
+    res.redirect('back');
+  }
 }
 
 module.exports = router;
