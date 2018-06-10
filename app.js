83e520e79f703b3caec61c0d13910f056db08f00
Pranay Maharana <pranay@Pranays-MBP.tampabay.rr.com>
2018-06-09 20:38:52 -0400

    updated DB url

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const commentRoutes     = require('./routes/comments'),
       campgroundRoutes  = require('./routes/campgrounds'),
       indexRoutes        = require('./routes/index');
 
-// mongoose.connect('mongodb://localhost/yelp_camp');
-mongoose.connect('mongodb://pranyeWest:gallickGun42@ds153890.mlab.com:53890/yelpcampu');
+// // mongoose.connect('mongodb://localhost/yelp_camp');
+mongoose.connect(process.env.DATABASEURL);
 //mongodb://pranyeWest:gallickGun42@ds153890.mlab.com:53890/yelpcampu
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
